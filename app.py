5319fe7f69bc4a2dac4c91bf8c43b8ec6a8898d4
kiri3l <klubimoff@gmail.com>
2020-05-16 19:16:36 +0300

    add html templates + data.py

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def render_main_page():
-    return "<h3>Тут будет главная страница</hl3>"
+    return render_template("index.html")
 
 
 @app.route("/departures/<departure>/")
 def render_departures(departure):
-    return "<h3>Тут будут направления</hl3>"
+    return render_template("departure.html")
 
 
 @app.route("/tours/<id>/")
 def render_tours(id):
-    return "<h3>Тут будут туры</hl3>"
+    return render_template("tours.html")
 
 
 app.run('0.0.0.0', 8000, debug=True)
